1597777511d5702019602177edca9db0485c3fad
mu99y <murugan.learning@gmail.com>
2021-10-04 06:04:14 +0100

    Initial commit

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Play.Catalog.Service.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        
+        
+    }
+}
